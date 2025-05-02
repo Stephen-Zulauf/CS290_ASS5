f030a665fd41ca46f2b71f973174ead6047d97ef
Stephen-Zulauf <szulauf1990@gmail.com>
2025-05-01 19:53:57 -0700

    finished part 1-added git ignore

--- assignment5/public/js/tableWriter.js
+++ assignment5/public/js/tableWriter.js
@@ -1,10 +1,8 @@
-import data from './data.js';
+import data from "./data.js";
 
 // Don't change anything above this comment
 
-
 // Feel free to define more functions if you want.
-
 
 /**
  * Create and return an HTML table element using the value of variable 'data.'
@@ -21,16 +19,40 @@ import data from './data.js';
  * nested array to create <td> elements in that row of the table.
  * @return HTML table element
  */
-function createTableElement(){
-    let tableElement = document.createElement('table');
-    // Add your code here to create the HTML table
+function createTableElement() {
+  let tableElement = document.createElement("table");
+  // Add your code here to create the HTML table
 
+  //create header for table
+  tableElement.appendChild(createRow(data[0], true));
 
-    // return the HTML table element
-    return tableElement;
+  //for each row in data- create and append new row to table element
+  for (let i = 1; i < data.length; i++) {
+    tableElement.appendChild(createRow(data[i], false));
+  }
+
+  return tableElement;
+}
+
+// creates and returns table row in dom. needs an array of data for the row.
+function createRow(content, isHeader) {
+  let row = document.createElement("tr");
+
+  content.forEach((e) => {
+    let td = null;
+    if (isHeader) {
+      td = document.createElement("th");
+    } else {
+      td = document.createElement("td");
+    }
+    td.appendChild(document.createTextNode(e));
+    row.appendChild(td);
+  });
+
+  return row;
 }
 
 // Don't change anything below this comment
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('dynamicTable').appendChild(createTableElement())
-})
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("dynamicTable").appendChild(createTableElement());
+});
