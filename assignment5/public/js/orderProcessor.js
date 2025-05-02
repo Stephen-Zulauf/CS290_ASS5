ccc73cc6c8d28a046755737326546020dd253a04
Stephen-Zulauf <szulauf1990@gmail.com>
2025-05-01 20:57:13 -0700

    everything working

--- assignment5/public/js/orderProcessor.js
+++ assignment5/public/js/orderProcessor.js
@@ -1,3 +1,4 @@
+//import { createRow } from "./tableWriter";
 /*
 Add an event handler for the submit event on the form with id "orderForm" in
     the file "order.html.
@@ -9,3 +10,36 @@ This event handler must:
 3. Append a new row to the table with the id "orderTable" in the file 
     "order.html." This row must contain the values entered by the user in the 2 input elements.
 */
+
+function createRow(content, isHeader) {
+  let row = document.createElement("tr");
+
+  content.forEach((e) => {
+    let td = null;
+    if (isHeader) {
+      td = document.createElement("th");
+    } else {
+      td = document.createElement("td");
+    }
+    td.appendChild(document.createTextNode(e));
+    row.appendChild(td);
+  });
+
+  return row;
+}
+
+function formHandle() {
+  const formElem = document.getElementById("orderForm");
+  formElem.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("form submitted");
+    let data = new FormData(formElem);
+    let table = document.getElementById("orderTable");
+    table.appendChild(createRow(data, false));
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM loaded");
+  formHandle();
+});
